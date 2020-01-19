7940b2fb9ffed3d2fc2b00d9edbba9eab210b7c6
Ghada BEN KHALIFA <benkhalifaghada@gmail.com>
2020-01-19 19:44:17 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Todoitem from './component/todoitem/Todoitem.js'
+
+function App() {
+  
+  return (
+    <div className="App">
+     <Todoitem />
+    
+       
+    </div>
+  );
+}
+
+export default App;
